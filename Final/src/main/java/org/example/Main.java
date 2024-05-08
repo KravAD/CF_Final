6113981076104b47aabc705a9a68a0a444ec6c94
KravAD <dkrav@myuax.com>
2024-05-08 10:58:49 +0200

    commit 8
    En la clase main instancio un Scanner para preguntar a los usuarios los datos del alias y email. Luego se instancia el método follow del usuario 2 al 1. El Scanner solicita el tweet del usuario 1.
    Por último pregunta el alias del usuario del que se quiere mostrar los datos.

--- Final/src/main/java/org/example/Main.java
+++ Final/src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 package org.example;
-import Ejercicio_4.*;
+import Ejercicio_3.*;
 import Ejercicio_5.*;
 import java.util.*;
 
@@ -7,10 +7,41 @@ import static Ejercicio_3.CalcRecursiva.recursive;
 
 public class Main {
     public static void main(String[] args) {
-int a = recursive(1, 0);
-System.out.println(a);
+        int a = recursive(1, 0);
+        System.out.println(a);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter alias for user1:");
+        String alias1 = scanner.nextLine();
+        System.out.println("Enter email for user1:");
+        String email1 = scanner.nextLine();
+        UserAccount user1 = new UserAccount(alias1, email1);
+
+        System.out.println("Enter alias for user2:");
+        String alias2 = scanner.nextLine();
+        System.out.println("Enter email for user2:");
+        String email2 = scanner.nextLine();
+        UserAccount user2 = new UserAccount(alias2, email2);
+
+        user1.follow(user2);
+
+        System.out.println("Enter tweet for user1:");
+        String tweetMessage = scanner.nextLine();
+        Tweet tweet1 = new Tweet(tweetMessage);
+        user1.tweet(tweet1);
+
+        List<UserAccount> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+
+        System.out.println("Enter the alias of the user you want to display:");
+        String displayAlias = scanner.nextLine();
+
+        for (UserAccount user : users) {
+            if (user.toString().contains(displayAlias)) {
+                System.out.println(user);
+            }
+        }
     }
-
-
-
 }
