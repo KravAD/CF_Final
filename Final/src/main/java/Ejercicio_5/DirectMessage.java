14fef107ea812a923020949660c7cd12d36df19e
KravAD <dkrav@myuax.com>
2024-05-08 11:14:38 +0200

    commit 14
    La clase DirectMessage tiene como atributo a un receptor. El constructor llama a super con los atributos de la clase Tweet y establece el parámetro reciever. El método toString devuelve: receptor, tiempo, mensaje y emisor.

--- Final/src/main/java/Ejercicio_5/DirectMessage.java
+++ Final/src/main/java/Ejercicio_5/DirectMessage.java
@@ -1,4 +1,22 @@
 package Ejercicio_5;
 
-public class DirectMessage extends Tweet{
+import java.time.LocalDate;
+
+public class DirectMessage extends Tweet {
+    private UserAccount receiver;
+
+    public DirectMessage(LocalDate time, String message, UserAccount sender, UserAccount receiver) {
+        super(time, message, sender);
+        this.receiver = receiver;
+    }
+
+    @Override
+    public String toString() {
+        return "DirectMessage{" +
+                "receiver=" + receiver +
+                ", time=" + time +
+                ", message='" + message + '\'' +
+                ", sender=" + sender +
+                '}';
+    }
 }
