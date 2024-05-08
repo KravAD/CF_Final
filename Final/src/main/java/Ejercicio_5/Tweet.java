2881e7f07570cb2ca405f4638f1a03acbb23a136
KravAD <dkrav@myuax.com>
2024-05-08 11:04:40 +0200

    commit 11
    Constructor de la clase

--- Final/src/main/java/Ejercicio_5/Tweet.java
+++ Final/src/main/java/Ejercicio_5/Tweet.java
@@ -5,10 +5,15 @@ import java.time.LocalDate;
 public class Tweet {
     protected LocalDate time;
     protected String message;
-    protected UserAccount user;
+    protected UserAccount sender;
 
-    public Tweet(String message) {
+    public Tweet(LocalDate time, String message, UserAccount sender) {
+        if (message.length() > 140) {
+            throw new IllegalArgumentException("Message exceeds the maximum length of 140 characters");
+        }
+        this.time = time;
         this.message = message;
+        this.sender = sender;
     }
 
     public String getMessage() {
