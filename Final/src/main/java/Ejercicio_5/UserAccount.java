15aeca4cd0f84a5a08e3ce876437653650aacea2
KravAD <dkrav@myuax.com>
2024-05-08 10:50:28 +0200

    Commit 7
    La clase UserAccount tiene como atributos dos String: alias y email.  Y otros cuatro atributos listas para los seguidores, seguidos, tweets y timeline.
    Tiene un constructor con dos condiciones que lanzan excepciones en caso de que el email o el alias estén expresados de forma incorrecta.
    Tiene tres métodos: el método follow tiene como argumento a user y añade a un seguidor, el método tweet tiene tweet como argumento y añade un nuevo tweet y una timeline y por último el toString que muestra el alias y email del user.

---
+++ Final/src/main/java/Ejercicio_5/UserAccount.java
@@ -0,0 +1,50 @@
+package Ejercicio_5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserAccount {
+    private String alias;
+    private String email;
+    private List<UserAccount> following;
+    private List<UserAccount> followers;
+    private List<Tweet> tweets;
+    private List<Tweet> timeline;
+
+    public UserAccount(String alias, String email) {
+        if (!Verificador.isValidEmail(email)) {
+            throw new IllegalArgumentException("Invalid email");
+        }
+        if (!Verificador.isValidAlias(alias)) {
+            throw new IllegalArgumentException("Invalid alias");
+        }
+        this.alias = alias;
+        this.email = email;
+        this.following = new ArrayList<>();
+        this.followers = new ArrayList<>();
+        this.tweets = new ArrayList<>();
+        this.timeline = new ArrayList<>();
+    }
+
+    public void follow(UserAccount user) {
+        if (!following.contains(user)) {
+            following.add(user);
+            user.followers.add(this);
+        }
+    }
+
+    public void tweet(Tweet tweet) {
+        tweets.add(tweet);
+        for (UserAccount follower : followers) {
+            follower.timeline.add(tweet);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "UserAccount{" +
+                "alias='" + alias + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
