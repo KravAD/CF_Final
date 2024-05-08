a26e82170a1016ba7c45cf41cdb4616335922b0f
KravAD <dkrav@myuax.com>
2024-05-08 11:12:28 +0200

    commit 13
    La clase Retweet tiene como atributo un un retweetedTweet. El constructor de la clase llama a super con los atributos de Tweet y atribuye el parametro retweetedTweet. Tambien se añade un toString.

--- Final/src/main/java/Ejercicio_5/Retweet.java
+++ Final/src/main/java/Ejercicio_5/Retweet.java
@@ -1,4 +1,23 @@
 package Ejercicio_5;
 
-public class Retweet extends Tweet
+import java.time.LocalDate;
+
+public class Retweet extends Tweet {
+    private Tweet retweetedTweet;
+
+    public Retweet(LocalDate time, String message, UserAccount sender, Tweet retweetedTweet) {
+        super(time, message, sender);
+        this.retweetedTweet = retweetedTweet;
+    }
+
+    @Override
+    public String toString() {
+        return "Retweet{" +
+                "retweetedTweet=" + retweetedTweet +
+                ", time=" + time +
+                ", message='" + message + '\'' +
+                ", sender=" + sender +
+                '}';
+    }
 }
+
