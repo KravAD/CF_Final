c05728e9dc1f4eab3f5b695f2628bad359531531
KravAD <dkrav@myuax.com>
2024-05-08 10:41:20 +0200

    commit 5
    Clase Tweet con un atributo mensaje con el constructor de la clase, getter y setter.

---
+++ Final/src/main/java/Ejercicio_5/Tweet.java
@@ -0,0 +1,17 @@
+package Ejercicio_5;
+
+public class Tweet {
+    private String message;
+
+    public Tweet(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
